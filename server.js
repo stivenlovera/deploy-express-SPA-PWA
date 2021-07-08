8f3f6047f1267311f71d35fb6c57569f415a2265
stiven lovera <50520255+stivenlovera@users.noreply.github.com>
2021-07-08 00:53:40 -0400

    pasado a respuesta estatica

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
+var path =require('path');
 var app = express();
 app.set('etag', false)
-app.use(express.static("./www/")); //dist/front-update
+app.use(express.static(path.join(__dirname,'www')));
 app.get("/", function (req, res, next) {
-  res.redirect("/");
+  res.sendFile(path.join(__dirname,'www/index.html'));
   
 });
 app.get("*", function (req, res, next) {
